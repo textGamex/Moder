ab4cc8069340657c2fcd8bfd8664db3dd164ff6c
textGamex <textGamex@outlook.com>
2024-09-12 00:05:25 +0800

    fix: 使用手写 RelayCommand 来避免IDE报错

--- Moder.Core/Models/ObservableGameValue.cs
+++ Moder.Core/Models/ObservableGameValue.cs
@@ -6,13 +6,15 @@ using Moder.Core.Models.Vo;
 
 namespace Moder.Core.Models;
 
-public abstract partial class ObservableGameValue(string key, NodeVo? parent) : ObservableObject
+public abstract class ObservableGameValue(string key, NodeVo? parent) : ObservableObject
 {
     public string Key { get; } = key;
     public bool IsChanged { get; private set; }
     public GameValueType Type { get; protected init; }
     public string TypeString => Type.ToString();
     public NodeVo? Parent { get; } = parent;
+    public IRelayCommand RemoveSelfInParentCommand => _removeSelfInParentCommand ??= new RelayCommand(RemoveSelfInParent);
+    private RelayCommand? _removeSelfInParentCommand;
 
     protected override void OnPropertyChanged(PropertyChangedEventArgs e)
     {
@@ -20,16 +22,16 @@ public abstract partial class ObservableGameValue(string key, NodeVo? parent) : 
         base.OnPropertyChanged(e);
     }
 
-    [RelayCommand]
     private void RemoveSelfInParent()
     {
         Debug.Assert(Parent != null, "Parent cannot be null");
         if (Parent is null)
         {
-	        return;
+            return;
         }
 
         Parent.Remove(this);
+        // 如果父节点下没有其他子节点，则删除父节点
         if (Parent.Children.Count == 0 && Parent.Parent is not null)
         {
             Parent.Parent.Remove(Parent);
