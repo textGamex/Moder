9708e9b137e0efe0f0f7b754e5024bd0dff787d2
textGamex <textGamex@outlook.com>
2024-09-25 01:33:36 +0800

    refactor: 优化StateCategory的实现

--- Moder.Core/Models/StateCategory/StateCategoryLeafVo.cs
+++ Moder.Core/Models/StateCategory/StateCategoryLeafVo.cs
@@ -1,38 +1,34 @@
-﻿using ParadoxPower.Parser;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Moder.Core.Services;
-using Microsoft.Extensions.DependencyInjection;
+using ParadoxPower.Parser;
 
 // ReSharper disable once CheckNamespace
 namespace Moder.Core.Models.Vo;
 
 public sealed partial class StateCategoryLeafVo(string key, Types.Value value, NodeVo? parent)
-	: LeafVo(key, value, parent)
+    : LeafVo(key, value, parent)
 {
-	public override string Value
-	{
-		get => _value;
-		set
-		{
-			SetProperty(ref _value, value);
-			OnPropertyChanged(nameof(StateCategoryUiDescription));
-		}
-	}
+    public override string Value
+    {
+        get => _value;
+        set
+        {
+            SetProperty(ref _value, value);
+            OnPropertyChanged(nameof(StateCategoryUiDescription));
+        }
+    }
 
-	public string StateCategoryUiDescription => GetStateCategoryUiDescription();
+    public string StateCategoryUiDescription => GetStateCategoryUiDescription();
 
-	public static IEnumerable<StateCategory> StateCategory { get; }
-	private static readonly StateCategoryService StateCategoryService;
+    public static IEnumerable<StateCategory> StateCategory => StateCategoryService.StateCategories;
+    private static readonly StateCategoryService StateCategoryService = App
+        .Current.Services.GetRequiredService<GameResourcesService>()
+        .StateCategory;
 
-	static StateCategoryLeafVo()
-	{
-		StateCategoryService = App.Current.Services.GetRequiredService<GameResourcesService>().StateCategory;
-		StateCategory = StateCategoryService.StateCategories;
-	}
-
-	private string GetStateCategoryUiDescription()
-	{
-		return StateCategoryService.TryGetValue(Value, out var stateCategory)
-			? $"建筑槽位 [{stateCategory.LocalBuildingSlots}]"
-			: $"未知的 {Key}";
-	}
+    private string GetStateCategoryUiDescription()
+    {
+        return StateCategoryService.TryGetValue(Value, out var stateCategory)
+            ? $"建筑槽位 [{stateCategory.LocalBuildingSlots}]"
+            : $"未知的 {Key}";
+    }
 }
