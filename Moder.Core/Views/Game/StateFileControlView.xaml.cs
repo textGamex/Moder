698b3243199ce334dc4719d3be012b9d2775bd7b
textGamex <textGamex@outlook.com>
2024-10-05 12:23:32 +0800

    Feat: 暂时将节点设置为不可拖动

--- Moder.Core/Views/Game/StateFileControlView.xaml.cs
+++ Moder.Core/Views/Game/StateFileControlView.xaml.cs
@@ -1,82 +1,156 @@
 using System.Collections;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using CommunityToolkit.WinUI;
+using Microsoft.Extensions.Logging;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Moder.Core.Controls;
 using Moder.Core.Models;
+using Moder.Core.Models.Vo;
 using Moder.Core.Services.GameResources;
 using Moder.Core.ViewsModels.Game;
+using Windows.Foundation;
+using Windows.Graphics;
+using WinUIEx;
 
 namespace Moder.Core.Views.Game;
 
 public sealed partial class StateFileControlView : IFileView
 {
-    public string Title => ViewModel.Title;
-    public string FullPath => ViewModel.FullPath;
-    public StateFileControlViewModel ViewModel => (StateFileControlViewModel)DataContext;
+	public string Title => ViewModel.Title;
+	public string FullPath => ViewModel.FullPath;
+	public StateFileControlViewModel ViewModel => (StateFileControlViewModel)DataContext;
 
-    private readonly GameResourcesService _gameResourcesService;
+	private readonly GameResourcesService _gameResourcesService;
+	private readonly DispatcherTimer _timer;
+	private readonly ILogger<StateFileControlView> _logger;
 
-    public StateFileControlView(StateFileControlViewModel model, GameResourcesService gameResourcesService)
-    {
-        _gameResourcesService = gameResourcesService;
+	public StateFileControlView(
+		StateFileControlViewModel model,
+		GameResourcesService gameResourcesService,
+		ILogger<StateFileControlView> logger
+	)
+	{
+		_gameResourcesService = gameResourcesService;
+		_logger = logger;
 
-        InitializeComponent();
-        DataContext = model;
-    }
+		_timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(50) };
+		_timer.Tick += (_, _) =>
+		{
+			GetCursorPos(out var point);
+			ScreenToClient(App.Current.MainWindow.GetWindowHandle(), ref point);
 
-    private async void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        // 确保 ItemsSource 不为空, 避免抛出异常
-        if (sender is ListView { ItemsSource: ICollection { Count: > 0 } } listView)
-        {
-            await listView.SmoothScrollIntoViewWithIndexAsync(
-                listView.SelectedIndex,
-                ScrollItemPlacement.Center,
-                false,
-                true
-            );
-        }
-    }
+			// TODO: 需要计算缩放比例
+			const double resolutionScale = 1.5;
+			var elements = VisualTreeHelper.FindElementsInHostCoordinates(
+				new Point(point.X / resolutionScale, point.Y / resolutionScale),
+				MainTreeView
+			);
+			if (
+				elements.Any(element =>
+				{
+					if (element is BaseLeaf)
+					{
+						return true;
+					}
 
-    private void AutoSuggestBox_OnSuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
-    {
-        Debug.Assert(args.SelectedItem is StateCategory);
+					if (element is TreeViewItem { Content: LeafValuesVo or LeafVo })
+					{
+						return true;
+					}
 
-        var stateCategory = (StateCategory)args.SelectedItem;
-        sender.Text = stateCategory.TypeName;
-    }
+					return false;
+				})
+			)
+			{
+				MainTreeView.CanReorderItems = false;
+			}
+			else
+			{
+				MainTreeView.CanReorderItems = true;
+			}
+		};
 
-    private void CountryTagAutoSuggestBox_OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
-    {
-        if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
-        {
-            var suggestions = SearchCountryTag(sender.Text);
+		InitializeComponent();
+		DataContext = model;
+	}
 
-            sender.ItemsSource = suggestions.Length > 0 ? suggestions : ["未找到"];
-        }
-    }
+	private async void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+	{
+		// 确保 ItemsSource 不为空, 避免抛出异常
+		if (sender is ListView { ItemsSource: ICollection { Count: > 0 } } listView)
+		{
+			await listView.SmoothScrollIntoViewWithIndexAsync(
+				listView.SelectedIndex,
+				ScrollItemPlacement.Center,
+				false,
+				true
+			);
+		}
+	}
 
-    private string[] SearchCountryTag(string query)
-    {
-        var countryTags = _gameResourcesService.CountryTagsService.CountryTags;
-        if (string.IsNullOrWhiteSpace(query))
-        {
-            return countryTags.ToArray();
-        }
+	private void AutoSuggestBox_OnSuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+	{
+		Debug.Assert(args.SelectedItem is StateCategory);
 
-        var suggestions = new List<string>(16);
+		var stateCategory = (StateCategory)args.SelectedItem;
+		sender.Text = stateCategory.TypeName;
+	}
 
-        foreach (var countryTag in countryTags)
-        {
-            if (countryTag.Contains(query, StringComparison.OrdinalIgnoreCase))
-            {
-                suggestions.Add(countryTag);
-            }
-        }
+	private void CountryTagAutoSuggestBox_OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+	{
+		if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+		{
+			var suggestions = SearchCountryTag(sender.Text);
 
-        return suggestions
-            .OrderByDescending(countryTag => countryTag.StartsWith(query, StringComparison.CurrentCultureIgnoreCase))
-            .ToArray();
-    }
+			sender.ItemsSource = suggestions.Length > 0 ? suggestions : ["未找到"];
+		}
+	}
+
+	private string[] SearchCountryTag(string query)
+	{
+		var countryTags = _gameResourcesService.CountryTagsService.CountryTags;
+		if (string.IsNullOrWhiteSpace(query))
+		{
+			return countryTags.ToArray();
+		}
+
+		var suggestions = new List<string>(16);
+
+		foreach (var countryTag in countryTags)
+		{
+			if (countryTag.Contains(query, StringComparison.OrdinalIgnoreCase))
+			{
+				suggestions.Add(countryTag);
+			}
+		}
+
+		return suggestions
+			.OrderByDescending(countryTag => countryTag.StartsWith(query, StringComparison.CurrentCultureIgnoreCase))
+			.ToArray();
+	}
+
+	private void TreeView_OnDragItemsCompleted(TreeView sender, TreeViewDragItemsCompletedEventArgs args)
+	{
+		_timer.Stop();
+		// _logger.LogDebug("DragItemsCompleted");
+	}
+
+	private void TreeView_OnDragItemsStarting(TreeView sender, TreeViewDragItemsStartingEventArgs args)
+	{
+		_timer.Start();
+
+		// TODO: 不能加入到 Leaf, LeafValue 中 |  加入一个 Node, 离开一个 Node, 调整位置
+	}
+
+	[DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+	private static extern bool GetCursorPos(out PointInt32 lpPoint);
+
+	[DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+	private static extern bool ScreenToClient(IntPtr hWnd, ref PointInt32 lpPoint);
+
+	[DllImport("gdi32.dll", SetLastError = true)]
+	private static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
 }
--- Moder.Core/ViewsModels/Game/StateFileControlViewModel.cs
+++ Moder.Core/ViewsModels/Game/StateFileControlViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Windows.Graphics.Display;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MethodTimer;
@@ -11,6 +12,7 @@ using Moder.Core.Services;
 using Moder.Core.ViewsModels.Menus;
 using ParadoxPower.Parser;
 using ParadoxPower.Process;
+using WinUIEx;
 using LeafValuesVo = Moder.Core.Models.Vo.LeafValuesVo;
 using LeafVo = Moder.Core.Models.Vo.LeafVo;
 
@@ -18,164 +20,164 @@ namespace Moder.Core.ViewsModels.Game;
 
 public sealed partial class StateFileControlViewModel : ObservableObject
 {
-    public IReadOnlyList<ObservableGameValue> Items => _rootNodeVo.Children;
-    public string Title => _fileItem.Name;
-    public string FullPath => _fileItem.FullPath;
-    public bool IsSuccess { get; }
+	public IReadOnlyList<ObservableGameValue> Items => _rootNodeVo.Children;
+	public string Title => _fileItem.Name;
+	public string FullPath => _fileItem.FullPath;
+	public bool IsSuccess { get; }
 
-    private readonly NodeVo _rootNodeVo = new("Root", null);
-    private readonly SystemFileItem _fileItem;
-    private readonly ILogger<StateFileControlViewModel> _logger;
-    private readonly LeafConverterService _leafConverterService;
+	private readonly NodeVo _rootNodeVo = new("Root", null);
+	private readonly SystemFileItem _fileItem;
+	private readonly ILogger<StateFileControlViewModel> _logger;
+	private readonly LeafConverterService _leafConverterService;
 
-    public StateFileControlViewModel(
-        GlobalResourceService resourceService,
-        ILogger<StateFileControlViewModel> logger,
-        LeafConverterService leafConverterService
-    )
-    {
-        _logger = logger;
-        _leafConverterService = leafConverterService;
-        _fileItem = resourceService.PopCurrentSelectFileItem();
-        Debug.Assert(_fileItem.IsFile);
+	public StateFileControlViewModel(
+		GlobalResourceService resourceService,
+		ILogger<StateFileControlViewModel> logger,
+		LeafConverterService leafConverterService
+	)
+	{
+		_logger = logger;
+		_leafConverterService = leafConverterService;
+		_fileItem = resourceService.PopCurrentSelectFileItem();
+		Debug.Assert(_fileItem.IsFile);
 
-        var timestamp = Stopwatch.GetTimestamp();
-        var parser = new TextParser(_fileItem.FullPath);
-        if (parser.IsFailure)
-        {
-            IsSuccess = false;
-            return;
-        }
+		var timestamp = Stopwatch.GetTimestamp();
+		var parser = new TextParser(_fileItem.FullPath);
+		if (parser.IsFailure)
+		{
+			IsSuccess = false;
+			return;
+		}
 
-        IsSuccess = true;
+		IsSuccess = true;
 
-        var rootNode = parser.GetResult();
-        var elapsedTime = Stopwatch.GetElapsedTime(timestamp);
-        logger.LogInformation("解析时间: {time} ms", elapsedTime.TotalMilliseconds);
+		var rootNode = parser.GetResult();
+		var elapsedTime = Stopwatch.GetElapsedTime(timestamp);
+		logger.LogInformation("解析时间: {time} ms", elapsedTime.TotalMilliseconds);
 
-        // 递归遍历所有节点
-        ConvertData(rootNode);
-    }
+		// 递归遍历所有节点
+		ConvertData(rootNode);
+	}
 
-    [Time("AST -> Vo")]
-    private void ConvertData(Node rootNode)
-    {
-        Debug.Assert(rootNode.Key == _fileItem.Name);
-        Convert(rootNode, _rootNodeVo);
-    }
+	[Time("AST -> Vo")]
+	private void ConvertData(Node rootNode)
+	{
+		Debug.Assert(rootNode.Key == _fileItem.Name);
+		Convert(rootNode, _rootNodeVo);
+	}
 
-    private void Convert(Node node, NodeVo nodeVo)
-    {
-        foreach (var child in node.AllArray)
-        {
-            if (child.IsLeafChild)
-            {
-                var leaf = child.leaf;
-                var leafVo = _leafConverterService.GetSpecificLeafVo(
-                    leaf.Key,
-                    leaf.Value.ToRawString(),
-                    leaf.Value.ToLocalValueType(),
-                    nodeVo
-                );
-                nodeVo.Add(leafVo);
-            }
+	private void Convert(Node node, NodeVo nodeVo)
+	{
+		foreach (var child in node.AllArray)
+		{
+			if (child.IsLeafChild)
+			{
+				var leaf = child.leaf;
+				var leafVo = _leafConverterService.GetSpecificLeafVo(
+					leaf.Key,
+					leaf.Value.ToRawString(),
+					leaf.Value.ToLocalValueType(),
+					nodeVo
+				);
+				nodeVo.Add(leafVo);
+			}
 
-            if (child.IsNodeChild)
-            {
-                var childNode = child.node;
-                // 当 LeafValues 不为空时，表示该节点是 LeafValues 节点
-                if (childNode.LeafValues.Any())
-                {
-                    nodeVo.Add(new LeafValuesVo(childNode.Key, childNode.LeafValues, nodeVo));
-                }
-                else
-                {
-                    // 是普通节点
-                    var childNodeVo = new NodeVo(childNode.Key, nodeVo);
-                    nodeVo.Add(childNodeVo);
-                    Convert(childNode, childNodeVo);
-                }
-            }
+			if (child.IsNodeChild)
+			{
+				var childNode = child.node;
+				// 当 LeafValues 不为空时，表示该节点是 LeafValues 节点
+				if (childNode.LeafValues.Any())
+				{
+					nodeVo.Add(new LeafValuesVo(childNode.Key, childNode.LeafValues, nodeVo));
+				}
+				else
+				{
+					// 是普通节点
+					var childNodeVo = new NodeVo(childNode.Key, nodeVo);
+					nodeVo.Add(childNodeVo);
+					Convert(childNode, childNodeVo);
+				}
+			}
 
-            // if (child.IsCommentChild)
-            // {
-            //     var comment = child.comment;
-            // }
-        }
-    }
+			// if (child.IsCommentChild)
+			// {
+			//     var comment = child.comment;
+			// }
+		}
+	}
 
-    [RelayCommand]
-    private void SaveData()
-    {
-        var parser = new TextParser(_fileItem.FullPath);
-        if (parser.IsFailure)
-        {
-            return;
-        }
+	[RelayCommand]
+	private void SaveData()
+	{
+		var parser = new TextParser(_fileItem.FullPath);
+		if (parser.IsFailure)
+		{
+			return;
+		}
 
-        // 与 Items 相同的层级结构
-        var rootNode = parser.GetResult();
-        var timestamp = Stopwatch.GetTimestamp();
-        // TODO: 数值有效性检查, int, float, bool
-        Save(rootNode, _rootNodeVo.Children.ToArray());
-        // var rootNode = SaveToNode(Items);
-        var elapsedTime = Stopwatch.GetElapsedTime(timestamp);
-        _logger.LogInformation("保存成功, 耗时: {time} ms", elapsedTime.TotalMilliseconds);
-        _logger.LogDebug(
-            "Content: {content}",
-            CKPrinter.PrettyPrintStatements(rootNode.AllArray.Select(child => child.GetRawStatement(rootNode.Key)))
-        );
-    }
+		// 与 Items 相同的层级结构
+		var rootNode = parser.GetResult();
+		var timestamp = Stopwatch.GetTimestamp();
+		// TODO: 数值有效性检查, int, float, bool
+		Save(rootNode, _rootNodeVo.Children.ToArray());
+		// var rootNode = SaveToNode(Items);
+		var elapsedTime = Stopwatch.GetElapsedTime(timestamp);
+		_logger.LogInformation("保存成功, 耗时: {time} ms", elapsedTime.TotalMilliseconds);
+		_logger.LogDebug(
+			"Content: {content}",
+			CKPrinter.PrettyPrintStatements(rootNode.AllArray.Select(child => child.GetRawStatement(rootNode.Key)))
+		);
+	}
 
-    private void Save(Node node, ObservableGameValue[] items)
-    {
-        var list = node.AllChildren;
-        for (var index = 0; index < list.Count; index++)
-        {
-            var child = list[index];
-            if (child.IsLeafChild || child.IsNodeChild || child.IsLeafValueChild)
-            {
-                var item = Array.Find(items, i => i.Key == child.GetKey());
-                if (item is not null)
-                {
-                    if (item is LeafVo { IsChanged: true } leafVo)
-                    {
-                        if (!node.TryGetLeaf(leafVo.Key, out var rawLeaf))
-                        {
-                            _logger.LogWarning("找不到Leaf: {key}", leafVo.Key);
-                            continue;
-                        }
+	private void Save(Node node, ObservableGameValue[] items)
+	{
+		var list = node.AllChildren;
+		for (var index = 0; index < list.Count; index++)
+		{
+			var child = list[index];
+			if (child.IsLeafChild || child.IsNodeChild || child.IsLeafValueChild)
+			{
+				var item = Array.Find(items, i => i.Key == child.GetKey());
+				if (item is not null)
+				{
+					if (item is LeafVo { IsChanged: true } leafVo)
+					{
+						if (!node.TryGetLeaf(leafVo.Key, out var rawLeaf))
+						{
+							_logger.LogWarning("找不到Leaf: {key}", leafVo.Key);
+							continue;
+						}
 
-                        rawLeaf.Value = leafVo.ToRawValue();
-                    }
+						rawLeaf.Value = leafVo.ToRawValue();
+					}
 
-                    if (item is LeafValuesVo { IsChanged: true } leafValuesVo)
-                    {
-                        if (!node.TryGetChild(leafValuesVo.Key, out var rawLeafVales))
-                        {
-                            _logger.LogWarning("找不到LeafValues: {key}", leafValuesVo.Key);
-                            continue;
-                        }
+					if (item is LeafValuesVo { IsChanged: true } leafValuesVo)
+					{
+						if (!node.TryGetChild(leafValuesVo.Key, out var rawLeafVales))
+						{
+							_logger.LogWarning("找不到LeafValues: {key}", leafValuesVo.Key);
+							continue;
+						}
 
-                        rawLeafVales.AllArray = leafValuesVo.ToLeafValues();
-                    }
+						rawLeafVales.AllArray = leafValuesVo.ToLeafValues();
+					}
 
-                    if (item is NodeVo nodeVo)
-                    {
-                        Save(
-                            // Array.Find(node.AllArray, child => child.IsNodeChild && child.node.Key == nodeVo.Key).node,
-                            list.Find(c => c.IsNodeChild && c.node.Key == nodeVo.Key).node,
-                            nodeVo.Children.ToArray()
-                        );
-                    }
-                }
-                else
-                {
-                    list.RemoveAt(index--);
-                }
-            }
-        }
+					if (item is NodeVo nodeVo)
+					{
+						Save(
+							// Array.Find(node.AllArray, child => child.IsNodeChild && child.node.Key == nodeVo.Key).node,
+							list.Find(c => c.IsNodeChild && c.node.Key == nodeVo.Key).node,
+							nodeVo.Children.ToArray()
+						);
+					}
+				}
+				else
+				{
+					list.RemoveAt(index--);
+				}
+			}
+		}
 
-        node.AllArray = list.ToArray();
-    }
+		node.AllArray = list.ToArray();
+	}
 }
