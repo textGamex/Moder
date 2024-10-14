7a69ca66c8258b4e7a0cd71dd6bfd04d9e7222c5
QAQWQAQWQ <53431397+QAQWQAQWQ@users.noreply.github.com>
2024-10-14 21:39:54 +0800

    Style BmpParser.cs

--- Moder.Core/Parser/BmpParser.cs
+++ Moder.Core/Parser/BmpParser.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Xml.Linq;
 using Moder.Core.Services.Config;
@@ -16,61 +16,62 @@ public class BmpParser
     public bool IsSuccess { get; }
 
     public bool IsFailure => !IsSuccess;
-    public ushort BmpType;
-    public uint BmpSize;
-    public ushort BmpReserved1;
-    public ushort BmpReserved2;
-    public uint BmpOffbits;
-    public uint BmpBitsize;
-    public uint BmpWide;
-    public uint BmpHeight;
-    public ushort BmpPlanes;
-    public ushort BmpBitcount;
-    public uint BmpCompression;
-    public uint BmpSizeImage;
-    public uint BmpXPelsPerMeter;
-    public uint BmpYPelsPerMeter;
-    public uint BmpClrUsed;
-    public uint BmpClrImportant;
-    public byte[,,] BmpColors = new byte[5000, 5000, 3];//R,G,B
+    public ushort Type;
+    public uint Size;
+    public ushort Reserved1;
+    public ushort Reserved2;
+    public uint Offbits;
+    public uint Bitsize;
+    public uint Wide;
+    public uint Height;
+    public ushort Planes;
+    public ushort Bitcount;
+    public uint Compression;
+    public uint SizeImage;
+    public uint XPelsPerMeter;
+    public uint YPelsPerMeter;
+    public uint ClrUsed;
+    public uint ClrImportant;
+    public byte[,,] Colors = new byte[5000, 5000, 3];//R,G,B
     public BmpParser(string filePath)
     {
         FilePath = File.Exists(filePath) ? filePath : throw new FileNotFoundException($"找不到文件: {filePath}", filePath);
         var fileName = Path.GetFileName(filePath);
-        FileStream BmpStream = new FileStream(filePath,FileMode.Open);
-        BinaryReader BmpBinaryReader = new BinaryReader(BmpStream);
-        BmpType=BmpBinaryReader.ReadUInt16();
-        BmpSize = BmpBinaryReader.ReadUInt32();
-        BmpReserved1 = BmpBinaryReader.ReadUInt16();
-        BmpReserved2 = BmpBinaryReader.ReadUInt16();
-        BmpOffbits = BmpBinaryReader.ReadUInt32();
-        BmpBitsize = BmpBinaryReader.ReadUInt32();
-        BmpWide = BmpBinaryReader.ReadUInt32();
-        BmpHeight = BmpBinaryReader.ReadUInt32();
-        BmpPlanes = BmpBinaryReader.ReadUInt16();
-        BmpBitcount = BmpBinaryReader.ReadUInt16();
-        BmpCompression = BmpBinaryReader.ReadUInt32();
-        BmpSizeImage = BmpBinaryReader.ReadUInt32();
-        BmpXPelsPerMeter = BmpBinaryReader.ReadUInt32();
-        BmpYPelsPerMeter = BmpBinaryReader.ReadUInt32();
-        BmpClrUsed = BmpBinaryReader.ReadUInt32();
-        BmpClrImportant = BmpBinaryReader.ReadUInt32();
-        if (BmpType != 0x4d42) { throw new Exception("BmpType Error"); }
-        if (BmpReserved1!=0) { throw new Exception("BmpReserved1 Error"); }
-        if (BmpReserved2 != 0) { throw new Exception("BmpReserved2 Error"); }
-        if (BmpPlanes != 1) { throw new Exception("BmpPlanes Error"); }
-        if (BmpCompression != 0) { throw new Exception("BmpCompression Error"); }
-        if (BmpWide > 4000 || BmpHeight > 4000) { throw new Exception("Bmp too large"); }
-        if (BmpBitcount!=24) { throw new Exception("Bmp没有使用24位保存"); }
-        for (uint i = 0; i < BmpHeight; i++) 
-        for (uint j = 0; j < BmpWide; j++){
+        FileStream bmpStream = new FileStream(filePath,FileMode.Open);
+        BinaryReader bmpBinaryReader = new BinaryReader(bmpStream);
+        Type=bmpBinaryReader.ReadUInt16();
+        Size = bmpBinaryReader.ReadUInt32();
+        Reserved1 = bmpBinaryReader.ReadUInt16();
+        Reserved2 = bmpBinaryReader.ReadUInt16();
+        Offbits = bmpBinaryReader.ReadUInt32();
+        Bitsize = bmpBinaryReader.ReadUInt32();
+        Wide = bmpBinaryReader.ReadUInt32();
+        Height = bmpBinaryReader.ReadUInt32();
+        Planes = bmpBinaryReader.ReadUInt16();
+        Bitcount = bmpBinaryReader.ReadUInt16();
+        Compression = bmpBinaryReader.ReadUInt32();
+        SizeImage = bmpBinaryReader.ReadUInt32();
+        XPelsPerMeter = bmpBinaryReader.ReadUInt32();
+        YPelsPerMeter = bmpBinaryReader.ReadUInt32();
+        ClrUsed = bmpBinaryReader.ReadUInt32();
+        ClrImportant = bmpBinaryReader.ReadUInt32();
+        if (Type != 0x4d42) { throw new Exception("Type Error"); }
+        if (Reserved1!=0) { throw new Exception("Reserved1 Error"); }
+        if (Reserved2 != 0) { throw new Exception("Reserved2 Error"); }
+        if (Planes != 1) { throw new Exception("Planes Error"); }
+        if (Compression != 0) { throw new Exception("Compression Error"); }
+        if (Wide > 4000 || Height > 4000) { throw new Exception("File too large"); }
+        if (Bitcount!=24) { throw new Exception("没有使用24位保存"); }
+        for (uint i = 0; i < Height; i++) 
+        for (uint j = 0; j < Wide; j++){
                 byte[] ThisPixel=new byte[3];
-                BmpBinaryReader.Read(ThisPixel, 0, 3);
-                BmpColors[i,j,0] = ThisPixel[0];
-                BmpColors[i,j,1] = ThisPixel[1];
-                BmpColors[i,j,2] = ThisPixel[2];
+                bmpBinaryReader.Read(ThisPixel, 0, 3);
+                Colors[i,j,0] = ThisPixel[0];
+                Colors[i,j,1] = ThisPixel[1];
+                Colors[i,j,2] = ThisPixel[2];
             }
-        return;
+        bmpStream.Dispose();
+        bmpBinaryReader.Dispose();
     }
     /*
     public void WriteTo(string OutputPath)
