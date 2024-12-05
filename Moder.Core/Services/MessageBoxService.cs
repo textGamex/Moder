9b06c7523df0a9c4920550509fd1633190f48cb9
textGamex <textGamex@outlook.com>
2024-12-06 00:47:49 +0800

    perf: MessageBox 带有对应等级的图标

--- Moder.Core/Services/MessageBoxService.cs
+++ Moder.Core/Services/MessageBoxService.cs
@@ -1,5 +1,6 @@
 using Moder.Language.Strings;
 using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 
 namespace Moder.Core.Services;
 
@@ -7,13 +8,23 @@ public sealed class MessageBoxService
 {
     public async Task WarnAsync(string message)
     {
-        var dialog = MessageBoxManager.GetMessageBoxStandard(Resource.Common_Warning, message);
+        var dialog = MessageBoxManager.GetMessageBoxStandard(
+            Resource.Common_Warning,
+            message,
+            ButtonEnum.Ok,
+            Icon.Warning
+        );
         await dialog.ShowAsync();
     }
 
     public async Task ErrorAsync(string message)
     {
-        var dialog = MessageBoxManager.GetMessageBoxStandard(Resource.Common_Error, message);
+        var dialog = MessageBoxManager.GetMessageBoxStandard(
+            Resource.Common_Error,
+            message,
+            ButtonEnum.Ok,
+            Icon.Error
+        );
         await dialog.ShowAsync();
     }
 }
