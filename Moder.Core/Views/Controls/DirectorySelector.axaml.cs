b24add1a623253a315143abc41922605895a3ede
Supheria <77865986+Supheria@users.noreply.github.com>
2024-12-10 22:44:30 +0800

    Refactor: 使用模板控件重用路径选择器 (#42)

---
+++ Moder.Core/Views/Controls/DirectorySelector.axaml.cs
@@ -0,0 +1,44 @@
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+
+namespace Moder.Core.Views.Controls;
+
+public sealed class DirectorySelector : TemplatedControl
+{
+    public string SelectorCaption
+    {
+        get => GetValue(SelectorCaptionProperty);
+        set => SetValue(SelectorCaptionProperty, value);
+    }
+    public static readonly StyledProperty<string> SelectorCaptionProperty
+    = AvaloniaProperty.Register<DirectorySelector, string>(nameof(SelectorCaption));
+    
+    public string DirectoryPath
+    {
+        get => GetValue(DirectoryPathProperty);
+        set => SetValue(DirectoryPathProperty, value);
+    }
+    public static readonly StyledProperty<string> DirectoryPathProperty = AvaloniaProperty.Register<
+        DirectorySelector,
+        string
+    >(nameof(DirectoryPath), enableDataValidation:true);
+
+    public ICommand SelectDirectoryCommand
+    {
+        get => GetValue(SelectDirectoryCommandProperty);
+        set => SetValue(SelectDirectoryCommandProperty, value);
+    }
+    public static readonly StyledProperty<ICommand> SelectDirectoryCommandProperty =
+        AvaloniaProperty.Register<DirectorySelector, ICommand>(nameof(SelectDirectoryCommand));
+    
+    protected override void UpdateDataValidation(AvaloniaProperty property, BindingValueType state, Exception? error)
+    {
+        if (property == DirectoryPathProperty)
+        {
+            DataValidationErrors.SetError(this, error);
+        }
+    }
+}
