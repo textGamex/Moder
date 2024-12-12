b25578c0d310ceb241699800a5061bccaafe9e60
textGamex <textGamex@outlook.com>
2024-12-12 10:32:21 +0800

    refactor: 使用 Brushes 类中的静态属性替代自定义画刷

--- Moder.Core/Views/Menus/SideBarControlView.axaml.cs
+++ Moder.Core/Views/Menus/SideBarControlView.axaml.cs
@@ -18,8 +18,6 @@ public sealed partial class SideBarControlView : UserControl
     // BUG: 右键选中效果会和滚动条重合, 在实现拉伸文件树时尝试修复
     private readonly FAMenuFlyout _contextMenu;
     private TreeViewItem? _lastSelectedTreeViewItem;
-    private readonly SolidColorBrush _rightSelectedItemBorderBrush = new(Colors.CornflowerBlue);
-    private readonly SolidColorBrush _transparentBorderBrush = new(Colors.Transparent);
     private readonly Thickness _rightSelectedItemThickness = new(0.65);
     private readonly TabViewNavigationService _tabViewNavigation;
 
@@ -63,7 +61,7 @@ public sealed partial class SideBarControlView : UserControl
             _contextMenu.ShowAt(treeViewItem, true);
             _lastSelectedTreeViewItem = treeViewItem;
             treeViewItem.BorderThickness = _rightSelectedItemThickness;
-            treeViewItem.BorderBrush = _rightSelectedItemBorderBrush;
+            treeViewItem.BorderBrush = Brushes.CornflowerBlue;
         }
     }
 
@@ -71,7 +69,7 @@ public sealed partial class SideBarControlView : UserControl
     {
         if (_lastSelectedTreeViewItem is not null)
         {
-            _lastSelectedTreeViewItem.BorderBrush = _transparentBorderBrush;
+            _lastSelectedTreeViewItem.BorderBrush = Brushes.Transparent;
         }
     }
 
