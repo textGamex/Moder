7bdc79137f713a3824ebd5cac38423b77877af8f
textGamex <textGamex@outlook.com>
2024-12-27 16:13:45 +0800

    feat: 使用编辑器控件打开文本文件

---
+++ Moder.Core/Controls/TextEditorControl.axaml.cs
@@ -0,0 +1,125 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Avalonia.Controls;
+using Avalonia.Media;
+using AvaloniaEdit.TextMate;
+using Moder.Core.Editor;
+using Moder.Core.Models;
+
+namespace Moder.Core.Controls;
+
+public sealed partial class TextEditorControl : UserControl
+{
+    public string Text
+    {
+        get => TextEditor.Text;
+        set => TextEditor.Text = value;
+    }
+    private TextMate.Installation _installation;
+    private ParadoxRegistryOptions _options;
+
+    public TextEditorControl()
+    {
+        InitializeComponent();
+        InitializeTextEditor();
+
+        ActualThemeVariantChanged += (_, _) =>
+        {
+            _installation.SetTheme(_options.LoadTheme(ActualThemeVariant));
+        };
+    }
+
+    // TODO: 状态栏
+    [MemberNotNull(nameof(_installation))]
+    [MemberNotNull(nameof(_options))]
+    private void InitializeTextEditor()
+    {
+        _options = new ParadoxRegistryOptions(App.Current.ActualThemeVariant);
+        TextEditor.Options.HighlightCurrentLine = true;
+        TextEditor.Options.EnableTextDragDrop = true;
+        TextEditor.TextArea.RightClickMovesCaret = true;
+        TextEditor.Options.ShowBoxForControlCharacters = true;
+        TextEditor.Options.AllowToggleOverstrikeMode = true;
+        TextEditor.ShowLineNumbers = true;
+        _installation = TextEditor.InstallTextMate(_options);
+
+        _installation.SetGrammar("source.hoi4");
+        _installation.AppliedTheme += ChangeThemeOnAppliedTheme;
+
+        ApplyThemeColorsToEditor(_installation);
+    }
+
+    private void ChangeThemeOnAppliedTheme(object? sender, TextMate.Installation e)
+    {
+        ApplyThemeColorsToEditor(e);
+    }
+
+    private void ApplyThemeColorsToEditor(TextMate.Installation installation)
+    {
+        ApplyBrushAction(installation, "editor.background", brush => TextEditor.Background = brush);
+        ApplyBrushAction(installation, "editor.foreground", brush => TextEditor.Foreground = brush);
+
+        if (
+            !ApplyBrushAction(
+                installation,
+                "editor.selectionBackground",
+                brush => TextEditor.TextArea.SelectionBrush = brush
+            )
+        )
+        {
+            if (App.Current.TryGetResource("TextAreaSelectionBrush", out var resourceObject))
+            {
+                if (resourceObject is IBrush brush)
+                {
+                    TextEditor.TextArea.SelectionBrush = brush;
+                }
+            }
+        }
+
+        if (
+            !ApplyBrushAction(
+                installation,
+                "editor.lineHighlightBackground",
+                brush =>
+                {
+                    TextEditor.TextArea.TextView.CurrentLineBackground = brush;
+                    TextEditor.TextArea.TextView.CurrentLineBorder = new Pen(brush);
+                }
+            )
+        )
+        {
+            TextEditor.TextArea.TextView.SetDefaultHighlightLineColors();
+        }
+
+        if (
+            !ApplyBrushAction(
+                installation,
+                "editorLineNumber.foreground",
+                brush => TextEditor.LineNumbersForeground = brush
+            )
+        )
+        {
+            TextEditor.LineNumbersForeground = TextEditor.Foreground;
+        }
+    }
+
+    private static bool ApplyBrushAction(
+        TextMate.Installation e,
+        string colorKeyNameFromJson,
+        Action<IBrush> applyColorAction
+    )
+    {
+        if (!e.TryGetThemeColor(colorKeyNameFromJson, out var colorString))
+        {
+            return false;
+        }
+
+        if (!Color.TryParse(colorString, out var color))
+        {
+            return false;
+        }
+
+        var colorBrush = new SolidColorBrush(color);
+        applyColorAction(colorBrush);
+        return true;
+    }
+}
---
+++ Moder.Core/Views/Game/FileEditorControlView.axaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Avalonia.Controls;
+using Moder.Core.Infrastructure;
+using Moder.Core.Models;
+
+namespace Moder.Core.Views.Game;
+
+public sealed partial class FileEditorControlView : UserControl, ITabViewItem
+{
+    public string Header => _fileItem.Name;
+    public string Id => _fileItem.FullPath;
+    public string ToolTip => _fileItem.FullPath;
+
+    private readonly SystemFileItem _fileItem;
+
+    public FileEditorControlView(SystemFileItem fileItem)
+    {
+        Debug.Assert(fileItem.IsFile);
+        _fileItem = fileItem;
+        InitializeComponent();
+
+        Editor.Text = File.ReadAllText(fileItem.FullPath, Encodings.Utf8NotBom);
+    }
+}
